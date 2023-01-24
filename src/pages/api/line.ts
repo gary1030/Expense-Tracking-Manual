5469c64081f907285f6a764d89c4997b87e14f16
gary1030 <gary6658@ntu.im>
2023-01-24 15:33:38 +0800

    Feature: list expenses

--- src/pages/api/line.ts
+++ src/pages/api/line.ts
@@ -2,11 +2,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getSpendingList, saveExpense } from '@/libs/sheet'
 
-const PORT = 3000
 const TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN
 
 const hintMessage = "欲登記花費，請輸入項目空一格輸入金額" + "\n\n" + "欲查詢今日花費，請輸入今日花費" + "\n\n" 
-+ "欲查詢本月花費，請輸入本月花費" + "\n\n" + "紀錄收入，請輸入收入空一格輸入金額"
++ "欲查詢本月花費，請輸入本月花費" + "\n\n"
 
 async function sendReplyMessage(replyToken: string, message: string) {
     if (!TOKEN || !replyToken) {
@@ -41,8 +40,7 @@ async function sendReplyMessage(replyToken: string, message: string) {
     await fetch("https://api.line.me/v2/bot/message/reply", webhookOptions)
 }
 
-
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse,
 ) {
@@ -53,55 +51,124 @@ export default function handler(
     }
 
     if (req.body.events && req.body.events[0].type === "message") {
-        var message = "hello"
+        var replyMessage = hintMessage
         // Parse events
         const userId = req.body.events[0].source.userId // User ID
         const messageText = req.body.events[0].message.text // Message text
         const timeStamp = req.body.events[0].timestamp // Timestamp
         
+        const today = new Date()
+
         switch (messageText) {
             case "hint":
-                message = hintMessage
                 break
             case "今日花費":
-                message = todayExpense()
+                const todayExpenses = await todayExpense(userId)
+                replyMessage = todayExpenses
                 break
-            case "今日消費":
-                message = todayExpense()
+            case "本月花費":
+                const thisMonthExpenses = await monthExpense(userId, today.getFullYear(), today.getMonth() + 1)
+                replyMessage = thisMonthExpenses
+                break
+            case "上月花費":
+                today.setMonth(today.getMonth() - 1)
+                const lastMonthExpenses = await monthExpense(userId, today.getFullYear(), today.getMonth() + 1)
+                replyMessage = lastMonthExpenses
+                break
+            case "本月花費清單":
+                const thisMonthExpensesList = await monthExpensesList(userId, today.getFullYear(), today.getMonth() + 1)
+                replyMessage = thisMonthExpensesList
+                break
+            case "上月花費清單":
+                today.setMonth(today.getMonth() - 1)
+                const lastMonthExpensesList = await monthExpensesList(userId, today.getFullYear(), today.getMonth() + 1)
+                replyMessage = lastMonthExpensesList
                 break
             default:
                 try {
                     var {expense, amount} = parseMessage(messageText)
                     if (expense && amount) {
                         saveExpense(userId, timeStamp, expense, amount)
-                        message = "Item: " + expense + "\n\n" + "Cost amount: " + amount + "\n\n" +"Record successfully!"
+                        replyMessage = "Item: " + expense + "\n\n" + "Cost amount: " + amount + "\n\n" +"Record successfully!"
                     }
                 } catch (error) {
-                    message = hintMessage
+                    replyMessage = hintMessage
                 }
                 break
         }
-
-        // Message data, must be stringified
         
-        sendReplyMessage(req.body.events[0].replyToken, message)
+        sendReplyMessage(req.body.events[0].replyToken, replyMessage)
     }
 
     res.status(200).json({ body: req.body, token: TOKEN })
 }
 
-function todayExpense() {
-    const sheets = getSpendingList()
-    
-    return "today"
+async function todayExpense(userId: string) {
+    const sheets = await getSpendingList()
+    const today = new Date()
+    const year = today.getFullYear()
+    const month = today.getMonth() + 1
+    const day = today.getDate()
+
+    if (!sheets) {
+        return "0"
+    }
+
+    const todayExpenses = sheets.filter((item) => 
+        item[0] == userId && item[5] == year && item[6] == month && item[7] == day
+    )
+
+    const total = todayExpenses.reduce((acc, item) => acc + Number(item[4]), 0)
+
+    return total.toString()
 }
 
-function monthExpense() {
-    return "month"
+async function monthExpense(userId: string, year: number, month: number) {
+    const sheets = await getSpendingList()
+
+    if (!sheets) {
+        return "0"
+    }
+
+    const monthExpenses = sheets.filter((item) => 
+        item[0] == userId && item[5] == year && item[6] == month
+    )
+
+    const total = monthExpenses.reduce((acc, item) => acc + Number(item[4]), 0)
+
+    return total.toString()
 }
 
-function recordExpense() {
-    return "record"
+async function monthExpensesList(userId: string, year: number, month: number) {
+    const sheets = await getSpendingList()
+
+    if (!sheets) {
+        return "Total: 0"
+    }
+
+    const monthExpenses = sheets.filter((item) => 
+        item[0] == userId && item[5] == year && item[6] == month
+    )
+
+    if (monthExpenses.length == 0) {
+        return "Total: 0"
+    }
+
+    var message = ""
+    var total = 0
+    var currentDay = monthExpenses[0][7]
+    message += month + "/" + currentDay + "\n"
+    monthExpenses.forEach((item) => {
+        if (currentDay != item[7]) {
+            currentDay = item[7]
+            message += "\n" + month + "/" + currentDay + "\n"
+        }
+        message += item[3] + " " + item[4] + "\n"
+        total += Number(item[4])
+    })
+
+    message += "\n" + "Total: " + total
+    return message
 }
 
 function parseMessage(message: string) {
